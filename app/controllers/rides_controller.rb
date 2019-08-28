4a8831ab82e42cd6b800c0fc7ae5f6b16e198135
Alan Krajina <alankrajina88@gmail.com>
2019-08-28 10:37:46 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,6 +4,7 @@ class RidesController < ApplicationController
 
   def create
     ride = Ride.create(user_id: current_user.id, attraction_id: params[:attraction_id])
+
     if ride
       response = ride.take_ride
       flash[:notice] = response
